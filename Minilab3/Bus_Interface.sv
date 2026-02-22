348db9078b7b517a17213aad3484c949e67e8306
TylerTHChen <tchen456@wisc.edu>
2026-02-22 01:43:52 -0600

    finished Bus_Interface.sv

--- Minilab3/Bus_Interface.sv
+++ Minilab3/Bus_Interface.sv
@@ -1,6 +1,22 @@
 module Bus_Interface(
-
+    input logic IOCS,
+    input logic IORW,
+    input logic RDA,
+    input logic TBR,
+    input logic [1:0] IOADDR,
+    input logic [7:0] recieve_buffer,
+    inout logic [7:0] DATABUS,
+    output logic [7:0] divisor_buffer
 );
 
+logic [7:0] status_reg;
+logic [7:0] databus_rd;
+
+// DATABUS logic
+assign status_reg =  {{6'b0}, TBR, RDA};
+assign databus_rd = IOADDR[0] ? status_ref : recieve_buffer;
+assign DATABUS = (IOCS && IORW) ? databus_rd : 8'bz;
+
+assign divisor_buffer = DATABUS;
 
 endmodule
