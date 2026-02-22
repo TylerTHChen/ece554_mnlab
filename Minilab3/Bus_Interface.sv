c2c0b557fb802bb8a8a31cac48b2b61282507e38
TylerTHChen <tchen456@wisc.edu>
2026-02-22 01:46:31 -0600

    update bus_interface to have same port name on spart

--- Minilab3/Bus_Interface.sv
+++ Minilab3/Bus_Interface.sv
@@ -1,22 +1,22 @@
-module Bus_Interface(
-    input logic IOCS,
-    input logic IORW,
-    input logic RDA,
-    input logic TBR,
-    input logic [1:0] IOADDR,
+module bus_interface(
+    input logic iocs,
+    input logic iorw,
+    input logic rda,
+    input logic tbr,
+    input logic [1:0] ioaddr,
     input logic [7:0] recieve_buffer,
-    inout logic [7:0] DATABUS,
+    inout logic [7:0] databus,
     output logic [7:0] divisor_buffer
 );
 
 logic [7:0] status_reg;
 logic [7:0] databus_rd;
 
-// DATABUS logic
-assign status_reg =  {{6'b0}, TBR, RDA};
-assign databus_rd = IOADDR[0] ? status_ref : recieve_buffer;
-assign DATABUS = (IOCS && IORW) ? databus_rd : 8'bz;
+// databus logic
+assign status_reg =  {{6'b0}, tbr, rda};
+assign databus_rd = ioaddr[0] ? status_ref : recieve_buffer;
+assign databus = (iocs && iorw) ? databus_rd : 8'bz;
 
-assign divisor_buffer = DATABUS;
+assign divisor_buffer = databus;
 
 endmodule
